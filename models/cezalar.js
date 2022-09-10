16fd593250ca6b9b5f22aec5440803763a5a9f18
Lwezy031 <88338961+Lwezy031@users.noreply.github.com>
2022-09-10 22:53:22 +0300

    Add files via upload

---
+++ models/cezalar.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const zade_cezalar = new mongoose.Schema({
+	user: String,
+	ihlal: Number,
+	yetkili: String,
+	ceza: String,
+	tarih: String,
+	bitiş: String,
+	sebep: String,
+	type: Number,
+});
+
+module.exports = mongoose.model("zade_cezalar", zade_cezalar);
